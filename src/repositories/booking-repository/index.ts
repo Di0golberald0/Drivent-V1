98573a272012fb8aa73e1b00c29c529fe93cadea
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2023-05-02 18:07:50 -0300

    feat: Entrega 4

---
+++ src/repositories/booking-repository/index.ts
@@ -0,0 +1,69 @@
+import { Booking, Room } from '@prisma/client';
+import { prisma } from '@/config';
+
+type CreateParams = Omit<Booking, 'id' | 'createdAt' | 'updatedAt'>;
+type UpdateParams = Omit<Booking, 'createdAt' | 'updatedAt'>;
+
+async function create({ roomId, userId }: CreateParams): Promise<Booking> {
+  return prisma.booking.create({
+    data: {
+      roomId,
+      userId,
+    },
+  });
+}
+
+async function findRoomCapacityById(id: number) {
+  return prisma.room.findMany({
+    where: {
+      id,
+    },
+  });
+}
+
+async function findByRoomId(roomId: number) {
+  return prisma.booking.findMany({
+    where: {
+      roomId,
+    },
+    include: {
+      Room: true,
+    },
+  });
+}
+
+async function findByUserId(userId: number) {
+  return prisma.booking.findFirst({
+    where: {
+      userId,
+    },
+    include: {
+      Room: true,
+    },
+  });
+}
+
+async function upsertBooking({ id, roomId, userId }: UpdateParams) {
+  return prisma.booking.upsert({
+    where: {
+      id,
+    },
+    create: {
+      roomId,
+      userId,
+    },
+    update: {
+      roomId,
+    },
+  });
+}
+
+const bookingRepository = {
+  create,
+  findRoomCapacityById,
+  findByRoomId,
+  findByUserId,
+  upsertBooking,
+};
+
+export default bookingRepository;
